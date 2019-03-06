4f1b61831da1fea5b2910bc2a06940915aa4669b
vikki <ivikki.dev@gmail.com>
2019-03-07 01:45:08 +0200

    mobile first

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 'use strict';
 
+let headerActivClass = document.getElementById('header');
+
+document.getElementById('hamburger').addEventListener('click', function (){
+    headerActivClass.classList.toggle('header-mobile');
+});
+
+window.addEventListener('resize', function(){
+    if(window.innerWidth >= 540){
+        headerActivClass.classList.remove('header-mobile');
+    }
+});

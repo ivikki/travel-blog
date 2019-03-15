49ebca071ae6e5089c7b2a15f585e43876efe947
vikki <ivikki.dev@gmail.com>
2019-03-15 15:53:39 +0200

    2 page

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-'use strict';
+"use strict";
 
-let headerActivClass = document.getElementById('header');
+let headerActiveClass = document.getElementById("header");
 
-document.getElementById('hamburger').addEventListener('click', function (){
-    headerActivClass.classList.toggle('header-mobile');
+document.getElementById("hamburger").addEventListener("click", function() {
+  headerActiveClass.classList.toggle("header-mobile");
 });
 
-window.addEventListener('resize', function(){
-    if(window.innerWidth >= 540){
-        headerActivClass.classList.remove('header-mobile');
-    }
+window.addEventListener("resize", function() {
+  if (window.innerWidth >= 540) {
+    headerActiveClass.classList.remove("header-mobile");
+  }
 });
 
-document.getElementById('js-top-page').addEventListener('click', function(){
-    window.scrollTo(0,0);
+document
+  .getElementsByClassName("nav-menu-link")
+  .addEventListener("click", function() {
+    document.getElementsByClassName("nav-menu-link").classList.toggle("active");
+  });
+
+document.getElementById("js-top-page").addEventListener("click", function() {
+  window.scrollTo(0, 0);
 });
